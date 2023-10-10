7ce83379e1ec0499b7e2fbfcd4f7590f01f91bf8
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-10-10 03:26:37 -0700

    new

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+class BaseModel():
+    """
+    defines all common attributes/methods for other classes
+    """
+    
+
+    def __init__(self):
+        """
+        method to take inistance from basemodel
+        """
+        pass
