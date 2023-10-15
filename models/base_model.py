46743edd5b51af80578584bebfd87cb1606ad098
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-10-15 05:50:50 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,16 @@ class BaseModel():
         """
         method to take inistance from basemodel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[k] = v
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
