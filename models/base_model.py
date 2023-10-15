af604639d096134c427adc73febaa8d1cf14e03f
OMNIA93 <omniannnnnnn@gmail.com>
2023-10-15 12:52:53 -0700

    new

--- models/base_model.py
+++ models/base_model.py
@@ -8,14 +8,25 @@ import os
 import sys
 import uuid
 
-class BaseModel():
+class BaseModel:
     """
     defines all common attributes/methods for other classes
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        method to take inistance from basemodel
+        """
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -37,7 +48,7 @@ class BaseModel():
         _dict__ of the instance
         """
         data = dict(self.__dict__)
-        data['__class__'] = self.__class__.__name__
-        data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.updated_at.isoformat()
+        data["__class__"] = self.__class__.__name__
+        data["created_at"] = self.created_at.isoformat()
+        data["updated_at"] = self.updated_at.isoformat()
         return data
