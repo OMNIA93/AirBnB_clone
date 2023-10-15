929757cc00c96d1abbd80e9e9dfee572b96a7499
OMNIA93 <omniannnnnnn@gmail.com>
2023-10-15 13:04:58 -0700

    consoleg

--- console.py
+++ console.py
@@ -13,35 +13,123 @@ from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the command line interpreter"""
-   
-    prompt = '(hbnb) '
-    my_dict = {
-        "BaseModel": BaseModel,
-        "User": User,
-        "State": State,
-        "City": City,
-        "Amenity": Amenity,
-        "Place": Place,
-        "Review": Review
-            }
+    prompt = "(hbnb) "
 
+    def do_create(self, arg):
+        """ Create a new instance of a BaseModel and save it """
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
 
-    def my_quit(self, arg):
-        """Quit command exits the program"""
-        return True
+    def do_show(self, arg):
+        """ Show string representation of an instance """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        if args[0] not in ["BaseModel", "User"]:  
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + "." + args[1]
+        if key in storage.all():
+            print(storage.all()[key])
+        else:
+            print("** no instance found **")
 
+    def do_destroy(self, arg):
+        """ Delete an instance based on the class name and id """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        if args[0] not in ["BaseModel", "User"]:  # قم بإضافة الفئات الأخرى هنا
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + "." + args[1]
+        if key in storage.all():
+            del storage.all()[key]
+            storage.save()
+        else:
+            print("** no instance found **")
 
-    def my_EOF(self, arg):
-        """EOF command exits the program"""
+    def do_all(self, arg):
+        """ Print all string representations of instances """
+        args = arg.split()
+        obj_list = []
+        if not arg:
+            for key, value in storage.all().items():
+                obj_list.append(str(value))
+            print(obj_list)
+            return
+        if args[0] not in ["BaseModel", "User"]:
+            print("** class doesn't exist **")
+            return
+        for key, value in storage.all().items():
+            if key.split(".")[0] == args[0]:
+                obj_list.append(str(value))
+        print(obj_list)
+
+    def do_update(self, arg):
+        """ Update an instance based on the class name and id """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        if args[0] not in ["BaseModel", "User"]:  
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + "." + args[1]
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        instance = storage.all()[key]
+        attr_name = args[2]
+        attr_value = args[3]
+        if hasattr(instance, attr_name):
+            attr_type = type(getattr(instance, attr_name))
+            if attr_type is str:
+                attr_value = str(attr_value)
+            elif attr_type is int:
+                try:
+                    attr_value = int(attr_value)
+                except ValueError:
+                    pass
+            elif attr_type is float:
+                try:
+                    attr_value = float(attr_value)
+                except ValueError:
+                    pass
+            setattr(instance, attr_name, attr_value)
+            storage.save()
+
+    def emptyline(self):
+        pass
+
+    def do_EOF(self, arg):
+        """ Exit the console """
         print("")
         return True
 
-
-    def emptyline(self):
-        """an empty line shouldn't execute anything"""
-        pass
-
-
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop(
