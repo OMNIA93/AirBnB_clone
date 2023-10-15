8cca6a8c6f3851b2e71fbddfabc439717ba0b06d
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-10-15 16:20:31 -0700

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,9 +3,14 @@
 import json
 import os
 
-from models import storage
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """Serializes instances to a JSON file and
