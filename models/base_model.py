a92fe3374bff7c460d6539c454206be2d3d12dec
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-10-16 17:10:50 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -19,12 +19,12 @@ class BaseModel():
         """
         method to take inistance from basemodel
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or k == "updated_at":
-                   setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                else:
-                     setattr(self, key, value)
+if kwargs:
+    for key, value in kwargs.items():
+        if key == "created_at" or key == "updated_at":
+            setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+        else:
+            setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -46,13 +46,9 @@ class BaseModel():
         self.updated_at = datetime.now()
         storage.save()
 
-    def to_dict(self):
-        """
-        returns a dictionary containing all keys/values of
-        _dict__ of the instance
-        """
-        make_dict = self.__dict__.copy()
-        make_dict["__class__"] = self.__class__.__name__
-        make_dict["created_at"] = self.created_at.isoformat()
-        make_dict["updated_at"] = self.updated_at.isoformat()
-        return make_dict
+def to_dict(self):
+    make_dict = self.__dict__.copy()
+    make_dict["__class__"] = self.__class__.__name__
+    make_dict["created_at"] = str(self.created_at)
+    make_dict["updated_at"] = str(self.updated_at)
+    return make_dict
