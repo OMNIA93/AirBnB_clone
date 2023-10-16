9365991519c4aa7d064664640a55155642b0f742
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-10-16 05:19:57 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,11 @@ class BaseModel():
         method to take inistance from basemodel
         """
         if kwargs:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+            for key, value in kwargs.items():
+                if key == "created_at" or k == "updated_at":
+                   setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
-                    self.__dict__[k] = v
+                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
