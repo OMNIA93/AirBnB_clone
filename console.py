c8b0198df277b53ef991b0c7975f4154cb8d6430
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-10-16 04:01:47 +0300

    Update console.py

--- console.py
+++ console.py
@@ -33,13 +33,13 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
     classes = {
-        "BaseModel": BaseModel(),
-        "User": User(),
-        "State": State(),
-        "City": City(),
-        "Amenity": Amenity(),
-        "Place": Place(),
-        "Review": Review()
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
     }
 
     def do_quit(self, arg):
