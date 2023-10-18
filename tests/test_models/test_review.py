e7801844122257cf28363c15beec146e78ac6b20
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-10-17 17:14:36 -0700

    test

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""Unittests for Amenity class"""
+"""Unittests for Review class"""
 
 import unittest
-from models.amenity import Amenity
-from models import amenity
+from models.review import Review
+from models import review
 
 
-class TestAmenity(unittest.TestCase):
-    """Test cases for Amenity Class"""
+class TestReview(unittest.TestCase):
+    """Test cases for Review Class"""
 
     def test_instantiation(self):
         """check for instance """
-        amen1 = Amenity()
-        self.assertIsInstance(amen1, Amenity)
-        self.assertTrue(hasattr(Amenity, "name"))
-        self.assertIsInstance(Amenity.name, str)
+        rev1 = Review()
+        self.assertIsInstance(rev1, Review)
+        self.assertTrue(hasattr(Review, "name"))
+        self.assertIsInstance(Review.name, str)
 
 
     def test_module_doc(self):
         """ check for module documentation """
-        self.assertTrue(len(amenity.__doc__) > 0)
+        self.assertTrue(len(review.__doc__) > 0)
 
     def test_class_doc(self):
         """ check for class documentation """
-        self.assertTrue(len(Amenity.__doc__) > 0)
+        self.assertTrue(len(Review.__doc__) > 0)
 
     def test_method_docs(self):
         """ check for method documentation """
-        for func in dir(Amenity):
+        for func in dir(Review):
             self.assertTrue(len(func.__doc__) > 0)
